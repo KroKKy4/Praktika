9d13934f1a2083738f1661dca3e30ed335fc075a
KroKKy4 <91180337+KroKKy4@users.noreply.github.com>
2022-06-02 10:04:27 +0300

    Update mytype.h

--- mytype.h
+++ mytype.h
@@ -12,7 +12,7 @@ typedef struct cars
 	int driving_skills;
 } bmw_m8;
 
-bmw_m8* Constructor(char* car_number, int fuel, int mileage, int tuning, int safety,int drifting,int driving_skills);
+bmw_m8* Constructor(char* car_number, int fuel, int mileage, int tuning, int safety, int drifting, int driving_skills);
 
 void Volume_of_engine(bmw_m8 bmw);
 
@@ -25,4 +25,3 @@ void race(bmw_m8 bmw);
 void drifting(bmw_m8 bmw);
 
 bmw_m8* Destructor(bmw_m8 bmw);
-
