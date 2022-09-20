b5d2cf85ebdc1e4489943e0b343a5f97e3ca39f4
KroKKy4 <91180337+KroKKy4@users.noreply.github.com>
2022-09-20 15:50:07 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,22 +1,16 @@
-#include "mytype.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <time.h>
+#include <math.h>
+#include "figure.h"
+#include "baza.h"
+#define _CRT_SECURE_NO_WARNINGS
 
 int main()
 {
-	bmw_m8* bmw = Constructor("228", 50, 25000, 0, 100, 0, 1);
-	bmw_m8* Constructor(char* car_number, int fuel, int mileage, int tuning, int safety, int drifting, int driving_skills);
-	Volume_of_engine(*bmw);
+	Base* base = BaseConstructor();
+	BasePrintStdout(base);
+	DeleteMultipleKeys(base);
+	BasePrintStdout(base);
 
-	tuning(*bmw);
-
-	Refuel(*bmw);
-
-	race(*bmw);
-
-	drifting(*bmw);
-
-	Destructor(*bmw);
+	return 0;
 }
